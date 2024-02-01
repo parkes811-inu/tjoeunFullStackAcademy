3b4345f65bf825436595e246c7efea6b461b8aea
parkes811 <parkes811@naver.com>
2024-02-01 18:08:31 +0900

    //2024-02-01 //MSA 기반 풀스택 교육 2일차

--- sample/src/DAY02/Ex14_TernaryOperator.java
+++ sample/src/DAY02/Ex14_TernaryOperator.java
@@ -4,7 +4,26 @@ public class Ex14_TernaryOperator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 3, b = 5;
+		
+		// a와 b 두 수의 차이(절댓값)를 구하시오.
+		// a - b = -2
+		// b - a = 2
+		
+		// 조건 연산자
+		int result = (a > b) ? a - b : b - a;
+		System.out.println("두 수의 차 : " + result);
 
+		// if문으로 표현
+		if(a > b) {
+			result = a - b;
+		}
+		else {
+			result = b - a;
+		}
+		
+		System.out.println("두 수의 차 : " + result);
+		
 	}
 
 }
